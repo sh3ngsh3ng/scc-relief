e03bca297bff22060366ffbde55735aa4de846bb
ys <ysdeveloper01@gmail.com>
2024-01-13 02:26:03 +0800

    added update for form

--- script.js
+++ script.js
@@ -6,6 +6,43 @@ document.addEventListener('DOMContentLoaded', function () {
         addTodo(todos, "Walk the dog", 5);
         addTodo(todos, "Clean the room", 3);
         addTodo(todos, "Pay the bill", 2);
+
+        const form = document.querySelector("#todo-form")
+        form.addEventListener('submit', function (event) {
+            event.preventDefault()
+            const taskNameInput = document.querySelector("#taskName")
+            const taskName = taskNameInput.value;
+
+            const taskUrgencySelect = document.querySelector("#taskUrgency");
+            const taskUrgency = taskUrgencySelect.value;
+
+            if (taskName) {
+                addTodo(todos, taskName, taskUrgency);
+                renderTodos(todos);
+                taskNameInput.value = '';
+            }
+        })
+
+
+        const todoList = document.querySelector("#todoList");
+        todoList.addEventListener("click", function (event) {
+            // console.log(event.target)
+            // Edit btn
+            if (event.target.classList.contains('edit-btn')) {
+                console.log(event.target.dataset.taskId)
+                const todoId = parseInt(event.target.dataset.taskId)
+                const todo = todos.find(t => t.id === todoId)
+
+                const newName = prompt("Enter the new task name: ", todo.name);
+                const newUrgency = prompt("Enter the new urgency: ", todo.urgency);
+                console.log("newName: ", newName);
+                console.log("newUrgency: ", newUrgency);
+
+                modifyTask(todos, todo.id, newName, newUrgency);
+                renderTodos(todos);
+            }
+
+        })
     }
 
     function renderTodos(todos) {
@@ -16,43 +53,43 @@ document.addEventListener('DOMContentLoaded', function () {
             li.className = 'list-group-item d-flex justify-content-between align-items-center';
             li.innerHTML = `
                                 ${todo.name} <span class="badge bg-primary">${todo.urgency}</span>               
-                                <button class="btn edit-btn btn-success btn-sm">Edit</button>
-                                <button class="btn delete-btn btn-danger btn-sm">Delete</button>
+                                <button data-task-id=${todo.id} class="btn edit-btn btn-success btn-sm">Edit</button>
+                                <button data-task-id=${todo.id} class="btn delete-btn btn-danger btn-sm">Delete</button>
                                 `;
             todoList.appendChild(li)
 
-            li.querySelector(".edit-btn").addEventListener('click', function () {
-                // alert("Editing: " + todo.name)
-                const newName = prompt("Enter the new task name: ", todo.name);
-                const newUrgency = prompt("Enter the new urgency: ", todo.urgency)
-                modifyTask(todos, todo.id, newName, newUrgency)
-                renderTodos(todos);
-            })
+            // li.querySelector(".edit-btn").addEventListener('click', function () {
+            //     // alert("Editing: " + todo.name)
+            //     const newName = prompt("Enter the new task name: ", todo.name);
+            //     const newUrgency = prompt("Enter the new urgency: ", todo.urgency)
+            //     modifyTask(todos, todo.id, newName, newUrgency)
+            //     renderTodos(todos);
+            // })
 
-            li.querySelector(".delete-btn").addEventListener('click', function () {
-                const confirmation = confirm("Do you want to delete the task: " + todo.name + "?");
-                if (confirmation) {
-                    deleteTask(todos, todo.id);
-                    renderTodos(todos);
-                }
-            });
+            // li.querySelector(".delete-btn").addEventListener('click', function () {
+            //     const confirmation = confirm("Do you want to delete the task: " + todo.name + "?");
+            //     if (confirmation) {
+            //         deleteTask(todos, todo.id);
+            //         renderTodos(todos);
+            //     }
+            // });
         }
     }
 
-    const addTodoButton = document.querySelector("#addTodo");
-    addTodoButton.addEventListener('click', function () {
-        const taskNameInput = document.querySelector("#taskName")
-        const taskName = taskNameInput.value;
+    // const addTodoButton = document.querySelector("#addTodo");
+    // addTodoButton.addEventListener('click', function () {
+    //     const taskNameInput = document.querySelector("#taskName")
+    //     const taskName = taskNameInput.value;
 
-        const taskUrgencySelect = document.querySelector("#taskUrgency");
-        const taskUrgency = taskUrgencySelect.value;
+    //     const taskUrgencySelect = document.querySelector("#taskUrgency");
+    //     const taskUrgency = taskUrgencySelect.value;
 
-        if (taskName) {
-            addTodo(todos, taskName, taskUrgency);
-            renderTodos(todos);
-            taskNameInput.value = '';
-        }
-    });
+    //     if (taskName) {
+    //         addTodo(todos, taskName, taskUrgency);
+    //         renderTodos(todos);
+    //         taskNameInput.value = '';
+    //     }
+    // });
 
 
     main();
